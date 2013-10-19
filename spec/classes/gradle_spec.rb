19f5cb960ff550d53c5062b2455b372632366297
sghill <sghill.dev@gmail.com>
2013-10-19 19:08:05 -0400

    add parameter for gradle daemon without trailing comma for puppet 2.6 compat

--- spec/classes/gradle_spec.rb
+++ spec/classes/gradle_spec.rb
@@ -27,5 +27,14 @@ describe 'gradle' do
       content.should include('export PATH="$PATH:/usr/local/gradle/bin"')
     end
   end
+
+  context 'no daemon' do
+    let(:params) { { :daemon => false } }
+
+    it 'should generate valid content' do
+      content = catalogue.resource('file', '/etc/profile.d/gradle.sh').send(:parameters)[:content]
+      content.should_not include('export GRADLE_OPTS="-Dorg.gradle.daemon=true"')
+    end
+  end
 end
 
